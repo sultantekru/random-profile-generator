913f9d8a0e45e87a224e309e420c1fb923f7ef70
sultantekru <tekru.1313@gmail.com>
2024-05-16 16:36:31 +0300

    Buton'a loading eklendi.

--- src/Profile/index.tsx
+++ src/Profile/index.tsx
@@ -1,25 +1,29 @@
 import React, { useEffect, useState } from 'react'
 import './style.css';
 import axios from 'axios';
+import { SpinnerGap } from '@phosphor-icons/react';
 
 export const Profile: React.FC = () => {
     const [profileImage, setProfileImage] = useState();
     const [profileEmail, setProfileEmail] = useState();
     const [profileName, setProfileName] = useState("");
     const [phoneNumber, setPhoneNumber] = useState();
+    const [isLoading, setIsLoading] = useState<boolean>(false);
 
     const generateProfile = () => {
+        setIsLoading(true);
         axios.get("https://randomuser.me/api").then(res => {
             console.log(res);
             setPhoneNumber(res.data.results[0].cell)
             setProfileEmail(res.data.results[0].email)
             setProfileImage(res.data.results[0].picture.large);
             setProfileName(`${res.data.results[0].name.first} ${res.data.results[0].name.last}`);
+            setIsLoading(false);
         })
     }
-    useEffect(()=>{
+    useEffect(() => {
         generateProfile()
-    },[])
+    }, [])
 
     return (
         <div className='profile-container'>
@@ -32,7 +36,9 @@ export const Profile: React.FC = () => {
                 <h4>{phoneNumber}</h4>
             </div>
             <div className="btn-container">
-                <button className='btn' onClick={generateProfile}>Generate Random Profile</button>
+                <button className='btn' style={ {justifyContent: isLoading ? 'space-between' : 'center'} } onClick={generateProfile}>
+                    {isLoading && <div className='loading-icon'><SpinnerGap size={32} /></div>}
+                    Generate Random Profile</button>
             </div>
         </div>
     )
